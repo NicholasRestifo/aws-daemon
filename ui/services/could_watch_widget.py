136a83855d721a72985ea4cbd8c2733721540fd5
Nick <nsrestifo@hotmail.com>
2025-09-07 19:52:28 -0400

    silly button implementations

--- ui/services/could_watch_widget.py
+++ ui/services/could_watch_widget.py
@@ -9,3 +9,15 @@ class CloudWatchWidget(QWidget):
         super().__init__()
         uic.loadUi(UI_FILE_PATH, self)
         self.console = console
+        self.button_1.clicked.connect(self.button_1_clicked)
+        self.button_2.clicked.connect(self.button_2_clicked)
+        self.button_3.clicked.connect(self.button_3_clicked)
+
+    def button_1_clicked(self):
+        self.console.handle_console_input("systeminfo")
+
+    def button_2_clicked(self):
+        self.console.handle_console_input("tasklist")
+
+    def button_3_clicked(self):
+        self.console.handle_console_input("help")
--- ui/services/iam_widget.py
+++ ui/services/iam_widget.py
@@ -9,3 +9,15 @@ class IamWidget(QWidget):
         super().__init__()
         uic.loadUi(UI_FILE_PATH, self)
         self.console = console
+        self.button_1.clicked.connect(self.button_1_clicked)
+        self.button_2.clicked.connect(self.button_2_clicked)
+        self.button_3.clicked.connect(self.button_3_clicked)
+
+    def button_1_clicked(self):
+        self.console.handle_console_input("dir")
+
+    def button_2_clicked(self):
+        self.console.handle_console_input("ls")
+
+    def button_3_clicked(self):
+        self.console.handle_console_input("where")
