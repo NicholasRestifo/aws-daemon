ab8857a340b705328652084923417b02de2bbb42
Nick <nsrestifo@hotmail.com>
2025-09-07 18:13:56 -0400

    resize input text

--- ui/console_input_widget.py
+++ ui/console_input_widget.py
@@ -1,4 +1,4 @@
-from PyQt6.QtCore import Qt, QEvent
+from PyQt6.QtCore import Qt, QSizeF
 from PyQt6.QtGui import QKeyEvent
 from PyQt6.QtWidgets import QTextEdit
 
@@ -8,6 +8,11 @@ class ConsoleInputWidget(QTextEdit):
     def __init__(self, parent=...):
         super().__init__(parent)
         self.model = None
+        self.document().documentLayout().documentSizeChanged.connect(self.adjust_height)
+
+    def adjust_height(self, size: QSizeF):
+        # Adding a small margin to prevent scrollbars appearing prematurely
+        self.setMaximumHeight(int(size.height()) + int(self.document().documentMargin() * 2))
 
     def keyReleaseEvent(self, event: QKeyEvent):
         if event.key() == Qt.Key.Key_Return:
