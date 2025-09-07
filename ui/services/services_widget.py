1ce577e450ed3ce7f947cf1035b91826e5bd8199
Nick <nsrestifo@hotmail.com>
2025-09-07 19:58:11 -0400

    update todo

--- ui/services/services_widget.py
+++ ui/services/services_widget.py
@@ -9,6 +9,6 @@ UI_FILE_PATH = "ui/services/services_form.ui"
 class ServicesWidget(QTabWidget):
     def __init__(self, console, parent):
         super().__init__(parent)
-        #todo pull names from data structure?
+        #todo pull names from data structure? move all this logic to a ui file?
         self.addTab(IamWidget(console), "IAM")
         self.addTab(CloudWatchWidget(console), "CloudWatch")
